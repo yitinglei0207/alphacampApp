ac94026e42c7e4fd3bc87af91b123c3613b4988d
yitinglei <yitinglei0207@gmail.com>
2015-05-02 12:03:39 +0800

    get class list done.

--- 0329AlphaCampAPP/ClassTableViewController.m
+++ 0329AlphaCampAPP/ClassTableViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ClassTableViewController.h"
+#import <AFNetworking/AFNetworking.h>
+#import "ApiKey.h"
 
-@interface ClassTableViewController ()
+@interface ClassTableViewController ()<UITableViewDataSource, UITableViewDelegate,
+UIActionSheetDelegate>
+@property(strong,nonatomic) NSArray *couseArray;
+@property(strong,nonatomic) NSArray *sectionArray;
+@property(strong,nonatomic) NSString *classTitle;
+@property (weak, nonatomic) IBOutlet UITableView *classTable;
 
 @end
 
@@ -18,9 +25,54 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    UIBarButtonItem *anotherButton = [[UIBarButtonItem alloc] initWithTitle:@"選擇課程" style:UIBarButtonItemStylePlain target:self action:@selector(void)];
-    self.navigationItem.rightBarButtonItem = anotherButton;
+    //    UIBarButtonItem *anotherButton = [[UIBarButtonItem alloc] initWithTitle:@"選擇課程" style:UIBarButtonItemStylePlain target:self action:@selector(void)];
+    //    self.navigationItem.rightBarButtonItem = anotherButton;
+    //
+    [self getAPIdata];
     
+}
+
+- (void) getAPIdata{
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    NSUserDefaults *userDefault = [NSUserDefaults standardUserDefaults];
+    NSString *authToken = [NSString stringWithFormat:@"%@",[userDefault stringForKey:@"auth_token"]];
+    
+    NSDictionary *userParameters = @{
+                                     @"api_key": api_key,
+                                     @"auth_token": authToken
+                                     };
+    [manager GET:@"https://dojo.alphacamp.co/api/v1/courses" parameters:userParameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+        _couseArray = responseObject[@"courses"];
+        _classTitle = [NSString stringWithFormat:@"%@", _couseArray[1][@"name"]];
+        NSString *courseID = [NSString stringWithFormat:@"%@", _couseArray[1][@"id"]];
+        NSString *sectionURL = [NSString stringWithFormat:@"https://dojo.alphacamp.co/api/v1/courses/%@",courseID];
+        
+        AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+        
+        NSDictionary *userParameter = @{
+                                        @"api_key": api_key,
+                                        @"auth_token": authToken
+                                        };
+        [manager GET:sectionURL parameters:userParameter success:^(AFHTTPRequestOperation *operation, id responseObject) {
+            _sectionArray = responseObject[@"syllabus"];
+            [_classTable reloadData];
+            
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            NSLog(@"Get Section Error: %@", error);
+        }];
+        
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Get course Error: %@", error);
+    }];
+    
+    
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [[self navigationController] setNavigationBarHidden:NO animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,7 +92,7 @@
 
 - (NSInteger)tableView:(UITableView*)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 2;
+    return _sectionArray.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -55,22 +107,20 @@
         cell.detailTextLabel.textColor = [UIColor lightGrayColor];
         
     } else {
-        NSLog(@"I have been initialized. Row = %li", (long)indexPath.row);
+        NSLog(@"I have been initialize. Row = %li", (long)indexPath.row);
     }
-    
-    if ((long)indexPath.row == 0) {
-        cell.textLabel.text = @"Week 1:認識AlphaCamp";
-        NSLog(@"I have been initialized. Row = %li", (long)indexPath.row);
-    }
-    else
-    {
-        cell.textLabel.text = @"Week 2:認識創業";
-        NSLog(@"I have been initialized. Row = %li", (long)indexPath.row);
-    }
+    cell.textLabel.text= [NSString stringWithFormat:@"%@",self.sectionArray[indexPath.row][@"section"][@"name"]];
     return cell;
 }
 
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return 44.0;
+}
 
+- (IBAction)couseSelect:(id)sender {
+    
+}
 
 
 
@@ -78,8 +128,8 @@
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 //- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.}
+// Get the new view controller using [segue destinationViewController].
+// Pass the selected object to the new view controller.}
 
 
 
